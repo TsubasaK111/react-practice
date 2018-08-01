62ff1039e66f63204336c4b1a3cc75a84e3ede7c
Code Chrysalis <codechrysalis@Codes-Mac-mini.local>
2018-08-01 16:43:00 +0900

    wip: Added dependencies for test.js.
    Co-authored-by: Tsubasa Kondo <tsubasa.k111@gmail.com>
    Co-authored-by: Hiroyuki Momoi <hirocorp01@gmail.com>

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,9 +1,8 @@
 // setup file
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
-
 
 jest.mock("../utils/");
 
